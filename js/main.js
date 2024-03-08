3ef2cc51c860090b3a69b3c6d041ea0c582e9d24
Mark Corbin <corb0140@algonquinlive.com>
2024-03-08 16:56:01 -0500

    changing way to call boolean complete

--- js/main.js
+++ js/main.js
@@ -4,10 +4,17 @@ import {
   css,
 } from "https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js";
 
+const data = [
+  { text: "Buy a burger", complete: false },
+  { text: "Eat a burger", complete: false },
+  { text: "Dream about a burger", complete: true },
+  { text: "Cook a burger", complete: true },
+];
+
 export class TodoItem extends LitElement {
   static properties = {
-    text: { attribute: false, type: String },
-    complete: { attribute: false, type: Boolean },
+    text: { type: String },
+    complete: { type: Boolean },
   };
 
   static styles = css`
@@ -48,38 +55,39 @@ export class TodoItem extends LitElement {
 
   constructor() {
     super();
-    this.text = [
-      "Buy a burger",
-      "Eat a burger",
-      "Dream about a burger",
-      "Cook a burger",
-    ];
-    this.complete = false;
+    this.data = data;
   }
 
   changeCompletionStatus(ev) {
-    if (ev.target.parentElement.parentElement.classList.contains("complete")) {
-      this.complete = true;
-    } else {
-      this.complete = false;
-    }
+    console.log(ev.target.parentElement.parentElement.getAttribute("index"));
+
+    // if (ev.target.parentElement.parentElement.classList.contains("complete")) {
+    //   this.complete = true;
+    // } else {
+    //   this.complete = false;
+    // }
   }
 
   toggleCompleteClass(ev) {
     if (ev.target) {
-      ev.target.parentElement.parentElement.classList.toggle("complete");
-
-      this.changeCompletionStatus(ev);
-      console.log(this.complete);
+      //get index of the item
+      // const index = ev.target.parentElement.parentElement;
+      // console.log(index);
     }
+
+    // if (ev.target) {
+    //   ev.target.parentElement.parentElement.classList.toggle("complete");
+    //   this.changeCompletionStatus(ev);
+    //   console.log(ev.target.parentElement.parentElement, this.complete);
+    // }
   }
 
   toggleImage(ev) {
-    if (this.complete === true) {
-      ev.target.src = "./imgs/cancel.svg";
-    } else {
-      ev.target.src = "./imgs/complete.svg";
-    }
+    // if (this.complete === true) {
+    //   ev.target.src = "./imgs/cancel.svg";
+    // } else {
+    //   ev.target.src = "./imgs/complete.svg";
+    // }
   }
 
   deleteItem(ev) {
@@ -88,11 +96,11 @@ export class TodoItem extends LitElement {
 
   render() {
     return html` <ul>
-      ${this.text.map(
-        (item) =>
+      ${this.data.map(
+        (content, index) =>
           html`
             <li>
-              ${item}
+              ${index + 1}: ${content.text}
 
               <div class="icons-container">
                 <img
