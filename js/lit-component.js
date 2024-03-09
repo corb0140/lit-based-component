39be303f7cf4c9b6c6bb050f6577e1881d8b1b61
Mark Corbin <corb0140@algonquinlive.com>
2024-03-09 14:02:13 -0500

    trying alternative method

---
+++ js/lit-component.js
@@ -0,0 +1,79 @@
+import {
+  LitElement,
+  html,
+} from "https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js";
+import { styles } from "./styles.js";
+
+const data = [
+  { text: "Buy a burger", complete: false },
+  { text: "Eat a burger", complete: false },
+  { text: "Dream about a burger", complete: true },
+  { text: "Cook a burger", complete: true },
+];
+
+export class TodoItem extends LitElement {
+  static properties = {
+    text: { type: String },
+    complete: { type: Boolean },
+  };
+
+  static styles = styles;
+
+  constructor() {
+    super();
+    this.data = data;
+  }
+
+  toggleCompleteClass(ev) {
+    if (ev.target) {
+      ev.target.parentElement.parentElement.classList.toggle("complete");
+    }
+  }
+
+  deleteItem(ev) {
+    ev.target.parentElement.parentElement.remove();
+  }
+
+  render() {
+    return html`
+      ${this.data.map(
+        (content, index) =>
+          html`
+            <li class=${content.complete === true ? "complete" : ""}>
+              ${index + 1}: ${content.text}
+
+              <div class="icons-container">
+                <img
+                  src=${content.complete === false
+                    ? "./imgs/complete.svg"
+                    : "./imgs/cancel.svg"}
+                  alt="complete icon or cancel icon"
+                  @click=${(ev) => {
+                    this.toggleCompleteClass(ev);
+
+                    if (ev.target) {
+                      content.complete = !content.complete;
+
+                      ev.target.src = content.complete
+                        ? "./imgs/cancel.svg"
+                        : "./imgs/complete.svg";
+                    }
+                  }}
+                />
+
+                <img
+                  src="./imgs/delete.svg"
+                  alt="trash can icon"
+                  @click=${(ev) => {
+                    this.deleteItem(ev);
+                  }}
+                />
+              </div>
+            </li>
+          `
+      )}
+    `;
+  }
+}
+
+// customElements.define("todo-item", TodoItem);
--- js/main.js
+++ js/main.js
@@ -1,79 +1,38 @@
-import {
-  LitElement,
-  html,
-} from "https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js";
-import { styles } from "./styles.js";
+import { TodoItem } from "./lit-component.js";
 
-const data = [
-  { text: "Buy a burger", complete: false },
-  { text: "Eat a burger", complete: false },
-  { text: "Dream about a burger", complete: true },
-  { text: "Cook a burger", complete: true },
-];
+const APP = {
+  todoList: document.querySelector("ul"),
+  todoItem: document.createElement("todo-item"),
+  data: [
+    { text: "Buy a burger", complete: false },
+    { text: "Eat a burger", complete: false },
+    { text: "Dream about a burger", complete: true },
+    { text: "Cook a burger", complete: true },
+  ],
 
-export class TodoItem extends LitElement {
-  static properties = {
-    text: { type: String },
-    complete: { type: Boolean },
-  };
+  init: () => {
+    console.log(APP.todoItem);
+    console.log(TodoItem);
+    console.log(APP.todoList);
 
-  static styles = styles;
+    APP.todoList.addEventListener("click", APP.handleItemClick);
 
-  constructor() {
-    super();
-    this.data = data;
-  }
+    APP.buildHtml;
+  },
 
-  toggleCompleteClass(ev) {
-    if (ev.target) {
-      ev.target.parentElement.parentElement.classList.toggle("complete");
-    }
-  }
+  handleItemClick: (ev) => {},
 
-  deleteItem(ev) {
-    ev.target.parentElement.parentElement.remove();
-  }
+  buildHtml: () => {
+    const df = document.createDocumentFragment();
 
-  render() {
-    return html` <ul>
-      ${this.data.map(
-        (content, index) =>
-          html`
-            <li class=${content.complete === true ? "complete" : ""}>
-              ${index + 1}: ${content.text}
+    APP.data.forEach((item) => {
+      APP.todoItem.text = item.text;
+      APP.todoItem.complete = item.complete;
+      df.appendChild(APP.todoItem);
+    });
 
-              <div class="icons-container">
-                <img
-                  src=${content.complete === false
-                    ? "./imgs/complete.svg"
-                    : "./imgs/cancel.svg"}
-                  alt="complete icon or cancel icon"
-                  @click=${(ev) => {
-                    this.toggleCompleteClass(ev);
+    APP.todoList.appendChild(df);
+  },
+};
 
-                    if (ev.target) {
-                      content.complete = !content.complete;
-
-                      ev.target.src = content.complete
-                        ? "./imgs/cancel.svg"
-                        : "./imgs/complete.svg";
-                    }
-                  }}
-                />
-
-                <img
-                  src="./imgs/delete.svg"
-                  alt="trash can icon"
-                  @click=${(ev) => {
-                    this.deleteItem(ev);
-                  }}
-                />
-              </div>
-            </li>
-          `
-      )}
-    </ul>`;
-  }
-}
-
-customElements.define("todo-item", TodoItem);
+document.addEventListener("DOMContentLoaded", APP.init);
