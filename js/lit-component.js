36512e1fdc9deb74841063a9fcc081a9fca75805
Mark Corbin <corb0140@algonquinlive.com>
2024-03-09 21:42:47 -0500

    major update to code format to utilize createElement and append

---
+++ js/lit-component.js
@@ -0,0 +1,62 @@
+import {
+  LitElement,
+  html,
+} from "https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js";
+import { styles } from "./styles.js";
+
+export class TodoItem extends LitElement {
+  static properties = {
+    index: { type: Number },
+    text: { type: String },
+    complete: { type: Boolean },
+  };
+
+  static styles = styles;
+
+  constructor() {
+    super();
+    this.index = 0;
+    this.text = "";
+    this.complete = false;
+  }
+
+  toggleCompleteClass(ev) {
+    if (ev.target) {
+      ev.target.parentElement.parentElement.classList.toggle("complete");
+    }
+  }
+
+  deleteItem(ev) {
+    ev.target.parentElement.parentElement.remove();
+  }
+
+  render() {
+    return html`<li class=${this.complete === true ? "complete" : ""}>
+      <span>${this.index}: ${this.text}</span>
+      <div class="icons-container">
+        <img
+          src=${this.complete === false
+            ? "./imgs/complete.svg"
+            : "./imgs/cancel.svg"}
+          alt="complete or cancel icon"
+          @click=${(ev) => {
+            this.toggleCompleteClass(ev);
+
+            if (ev.target) {
+              this.complete = !this.complete;
+              console.log(this.complete);
+            }
+          }}
+        />
+
+        <img
+          src="./imgs/delete.svg"
+          alt="trash can icon"
+          @click=${this.deleteItem}
+        />
+      </div>
+    </li>`;
+  }
+}
+
+customElements.define("todo-item", TodoItem);
